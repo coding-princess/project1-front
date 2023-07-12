4a6bec96473d8a798a06aa47791b28b8dd57ec39
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2023-07-13 03:56:58 +0900

    header footer mainPage login completed without event

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import Layout from "./Layout/Layout"
-import MainPage from './Page/MainPage'
-import './App.css';
+import HeaderTemplate from "./Components/Header"
+import FooterTemplate from "./Components/Footer"
+import MainJoin from "./Components/MainJoin"
+import Login from "./Components/Login"
 
+/* 질문: 화면 전환 어떻게 구현하는지 모르겠다. 예: 메인 화면 > (이벤트 발생) > 로그인 화면  */
 function App() {
   return (
-    <Layout>
-      <MainPage />
-    </Layout>
+    <div>
+      <HeaderTemplate />
+      <Login />
+      <FooterTemplate />
+    </div>
   );
 }
 
